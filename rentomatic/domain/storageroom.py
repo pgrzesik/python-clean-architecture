d70f7d200bbeb56f6c6c48c07ae0ddd5d1cbcf86
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-02 00:03:02 +0100

    add domain model for storageroom

---
+++ rentomatic/domain/storageroom.py
@@ -0,0 +1,17 @@
+from rentomatic.shared.domain_model import DomainModel
+
+
+class StorageRoom:
+    def __init__(self, code, size, price, latitude, longitude):
+        self.code = code
+        self.size = size
+        self.price = price
+        self.latitude = float(latitude)
+        self.longitude = float(longitude)
+
+    @classmethod
+    def from_dict(cls, dictionary):
+        room = cls(**dictionary)
+    
+
+DomainModel.register(StorageRoom)
