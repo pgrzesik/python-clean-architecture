7fd64ed05147b0c6db8b36b6043f6fc480fc0a2e
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-06 21:05:55 +0100

    implement ResponseSuccess

---
+++ rentomatic/shared/response_objects.py
@@ -0,0 +1,3 @@
+class ResponseSuccess():
+    def __init__(self, value=None):
+        self.value = value
--- tests/shared/test_response_objects.py
+++ tests/shared/test_response_objects.py
@@ -3,4 +3,4 @@ from rentomatic.shared import response_objects
 
 def test_response_success():
     res = response_objects.ResponseSuccess()
-    assert res
+    assert res.value is None
