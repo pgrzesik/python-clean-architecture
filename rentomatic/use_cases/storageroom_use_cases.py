462864971eb86610ce261ccdd63265186423cf90
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-06 21:11:15 +0100

    reimplement use case to use request and response objects

--- rentomatic/use_cases/storageroom_use_cases.py
+++ rentomatic/use_cases/storageroom_use_cases.py
@@ -1,7 +1,11 @@
+from rentomatic.shared.response_objects import ResponseSuccess
+
+
 class StorageRoomListUseCase:
 
     def __init__(self, repo):
         self.repo = repo
 
-    def execute(self):
-        return self.repo.list()
+    def execute(self, req):
+        storage_rooms = self.repo.list()
+        return ResponseSuccess(value=storage_rooms)
