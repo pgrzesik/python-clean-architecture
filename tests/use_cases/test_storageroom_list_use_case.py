50b0e4616282401b6ae086886ae54bd40d33c6af
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-06 20:47:12 +0100

    add test for listing use case

---
+++ tests/use_cases/test_storageroom_list_use_case.py
@@ -0,0 +1,36 @@
+from unittest import mock
+
+import pytest
+
+from rentomatic.domain.storageroom import StorageRoom
+from rentomatic.use_cases import storageroom_use_cases as uc
+
+
+@pytest.fixture
+def domain_storagerooms():
+    first_storageroom = StorageRoom(
+        code='123',
+        size=100,
+        price=10,
+        longitude='-0.0911111',
+        latitude='51.751111')
+
+    second_storageroom = StorageRoom(
+        code='456',
+        size=150,
+        price=20,
+        longitude='-0922222',
+        latitude='51.75222')
+
+    return [first_storageroom, second_storageroom]
+
+
+def test_storageroom_list(domain_storagerooms):
+    repo = mock.Mock()
+    repo.list.return_value = domain_storagerooms
+
+    storageroom_list_use_case = uc.StorageRoomListUseCase(repo)
+    result = storageroom_list_use_case.execute()
+
+    repo.list.assert_called_once_with()
+    assert result == domain_storagerooms
