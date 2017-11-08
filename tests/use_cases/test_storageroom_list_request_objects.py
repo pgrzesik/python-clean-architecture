a3386a0a253656bdc4844605b2f5fe285bc8bb2f
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-08 16:50:59 +0100

    add test for passing filters to request object

--- tests/use_cases/test_storageroom_list_request_objects.py
+++ tests/use_cases/test_storageroom_list_request_objects.py
@@ -5,9 +5,18 @@ def build_storageroom_list_request_object():
     req = request_objects.StorageRoomListRequestObject()
 
     assert req
+    assert req.filters is None
 
 
 def test_build_storageroom_list_request_object_from_dict():
     req = request_objects.StorageRoomListRequestObject.from_dict({})
 
     assert req
+    assert req.filters is None
+
+
+def test_build_storageroom_list_request_object_with_empty_filters():
+    req = request_objects.StorageRoomListRequestObject(filters={})
+
+    assert req
+    assert req.filters == {}
