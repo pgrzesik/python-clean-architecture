2a6729d7f34f17bbe92c6b9e404ab5bca0b5bf91
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-08 17:03:50 +0100

    refactor tests to group them in class TestStorageRoomListRequestObject

--- tests/use_cases/test_storageroom_list_request_objects.py
+++ tests/use_cases/test_storageroom_list_request_objects.py
@@ -1,28 +1,27 @@
-from rentomatic.use_cases import request_objects
+from rentomatic.use_cases.request_objects import StorageRoomListRequestObject
 
 
-def build_storageroom_list_request_object():
-    req = request_objects.StorageRoomListRequestObject()
+class TestStorageRoomListRequestObject:
+    def test_build(self):
+        req = StorageRoomListRequestObject()
 
-    assert req
-    assert req.filters is None
+        assert req
+        assert req.filters is None
 
+    def test_build_from_dict(self):
+        req = StorageRoomListRequestObject.from_dict({})
 
-def test_build_storageroom_list_request_object_from_dict():
-    req = request_objects.StorageRoomListRequestObject.from_dict({})
+        assert req
+        assert req.filters is None
 
-    assert req
-    assert req.filters is None
+    def test_build_with_empty_filters(self):
+        req = StorageRoomListRequestObject(filters={})
 
+        assert req
+        assert req.filters == {}
 
-def test_build_storageroom_list_request_object_with_empty_filters():
-    req = request_objects.StorageRoomListRequestObject(filters={})
+    def test_build_from_dict_with_empty_filters(self):
+        req = StorageRoomListRequestObject.from_dict(filters={})
 
-    assert req
-    assert req.filters == {}
-
-def test_build_storageroom_list_request_object_from_dict_with_empty_filters():
-    req = request_objects.StorageRoomListRequestObject.from_dict(filters={})
-
-    assert req
-    assert req.filters == {}
+        assert req
+        assert req.filters == {}
