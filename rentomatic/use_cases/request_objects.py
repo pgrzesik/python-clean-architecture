4ae77a3f6499cfbccadbcb3d24c10b6088bd4c3f
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-08 16:53:45 +0100

    implement handling filters while constructing request

--- rentomatic/use_cases/request_objects.py
+++ rentomatic/use_cases/request_objects.py
@@ -1,4 +1,8 @@
 class StorageRoomListRequestObject:
+
+    def __init__(self, filters=None):
+        self.filters = filters
+
     @classmethod
     def from_dict(cls, params):
         return StorageRoomListRequestObject()
