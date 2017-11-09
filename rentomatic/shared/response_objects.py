f2fa57f41128b2778a27299f94629e10297ecae7
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-09 16:33:08 +0100

    implement value for ResponseFailure

--- rentomatic/shared/response_objects.py
+++ rentomatic/shared/response_objects.py
@@ -11,3 +11,10 @@ class ResponseFailure:
     def __init__(self, type_, message):
         self.type = type_
         self.message = message
+
+    @property
+    def value(self):
+        return {
+            'type': self.type,
+            'message': self.message
+        }
