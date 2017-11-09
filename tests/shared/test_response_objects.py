5960700fcafc78929559a81ff5bff0b245b3d173
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-09 16:18:17 +0100

    rewrite tests to use value

--- tests/shared/test_response_objects.py
+++ tests/shared/test_response_objects.py
@@ -1,6 +1,26 @@
-from rentomatic.shared import response_objects
+import pytest
+
+from rentomatic.shared.response_objects import ResponseSuccess
 
 
-def test_response_success():
-    res = response_objects.ResponseSuccess()
-    assert res.value is None
+@pytest.fixture
+def response_value():
+    return {
+        'key': ['value', 'second_value']
+    }
+
+
+@pytest.fixture
+def response_type():
+    return 'ResponseError'
+
+
+@pytest.fixture
+def response_message():
+    return 'This is a response error'
+
+
+def test_response_success(response_value):
+    res = ResponseSuccess(response_value)
+
+    assert res.value == response_value
