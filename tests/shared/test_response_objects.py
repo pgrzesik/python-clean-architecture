e044b643c7210d2f32485a358817fec280014e07
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-09 16:40:05 +0100

    refactor tests to test classes

--- tests/shared/test_response_objects.py
+++ tests/shared/test_response_objects.py
@@ -20,23 +20,24 @@ def response_message():
     return 'This is a response error'
 
 
-def test_response_success(response_value):
-    res = ResponseSuccess(response_value)
+class TestResponseSuccess:
+    def test_response_success(self, response_value):
+        res = ResponseSuccess(response_value)
 
-    assert res.value == response_value
-
-
-def test_response_failure(response_type, response_message):
-    res = ResponseFailure(response_type, response_message)
-
-    assert res.type == response_type
-    assert res.message == response_message
+        assert res.value == response_value
 
 
-def test_response_failure_contains_value(response_type, response_message):
-    res = ResponseFailure(response_type, response_message)
+class TestResponseFailure:
+    def test_contains_type_and_message(self, response_type, response_message):
+        res = ResponseFailure(response_type, response_message)
 
-    assert res.value == {
-        'type': response_type,
-        'message': response_message
-    }
+        assert res.type == response_type
+        assert res.message == response_message
+
+    def test_contains_value(self, response_type, response_message):
+        res = ResponseFailure(response_type, response_message)
+
+        assert res.value == {
+            'type': response_type,
+            'message': response_message
+        }
