67927beee67f3ef5ed2063378989ca147be01c61
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-15 16:42:48 +0100

    add option to create ResponseFailure from exceptions

--- rentomatic/shared/response_objects.py
+++ rentomatic/shared/response_objects.py
@@ -10,7 +10,12 @@ class ResponseFailure:
 
     def __init__(self, type_, message):
         self.type = type_
-        self.message = message
+        self.message = self._format_message(message)
+
+    def _format_message(self, msg):
+        if isinstance(msg, Exception):
+            return f'{msg.__class__.__name__}: {msg}'
+        return msg
 
     @property
     def value(self):
