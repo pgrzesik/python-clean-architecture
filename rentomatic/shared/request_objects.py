3b0905bcbecd34303a01c7cfff15490ce0a71e44
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-15 21:19:49 +0100

    move base request objects to shared

---
+++ rentomatic/shared/request_objects.py
@@ -0,0 +1,28 @@
+class BaseRequestObject:
+    def is_valid(self):
+        raise NotImplemented
+
+
+class InvalidRequestObject:
+
+    def __init__(self):
+        self.errors = []
+
+    def add_error(self, parameter, message):
+        self.errors.append({'parameter': parameter, 'message': message})
+
+    def has_errors(self):
+        return len(self.errors) > 0
+
+    def is_valid(self):
+        return False
+
+
+class ValidRequestObject:
+
+    @classmethod
+    def from_dict(cls, params):
+        raise NotImplemented
+
+    def is_valid(self):
+        return True
--- rentomatic/use_cases/request_objects.py
+++ rentomatic/use_cases/request_objects.py
@@ -1,34 +1,7 @@
 import collections
 
-
-class BaseRequestObject:
-    def is_valid(self):
-        raise NotImplemented
-
-
-class InvalidRequestObject:
-
-    def __init__(self):
-        self.errors = []
-
-    def add_error(self, parameter, message):
-        self.errors.append({'parameter': parameter, 'message': message})
-
-    def has_errors(self):
-        return len(self.errors) > 0
-
-    def is_valid(self):
-        return False
-
-
-class ValidRequestObject:
-
-    @classmethod
-    def from_dict(cls, params):
-        raise NotImplemented
-
-    def is_valid(self):
-        return True
+from rentomatic.shared.request_objects import (
+    InvalidRequestObject, ValidRequestObject)
 
 
 class StorageRoomListRequestObject(ValidRequestObject):
