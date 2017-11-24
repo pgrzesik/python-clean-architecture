747bd099eff55ae35126374003ab9f7f4d907e66
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-24 23:29:01 +0100

    add helper for asserting results in memrepo tests

--- tests/repository/test_memrepo.py
+++ tests/repository/test_memrepo.py
@@ -1,6 +1,7 @@
 import pytest
 
 from rentomatic.repository.memrepo import MemRepo
+from rentomatic.shared.domain_model import DomainModel
 
 
 class TestMemrepo:
@@ -31,6 +32,12 @@ class TestMemrepo:
     @pytest.fixture
     def repo(self, storagerooms):
         return MemRepo(storagerooms)
+
+    def _assert_results(self, domain_models_list, data_list):
+        assert len(domain_models_list) == len(data_list)
+        assert all(isinstance(dm, DomainModel) for dm in domain_models_list)
+        assert set(dm.code for dm in domain_models_list) == \
+            set(d['code'] for d in data_list)
 
     def test_list_without_parameters(self, repo, storagerooms):
         assert repo.list() == storagerooms
