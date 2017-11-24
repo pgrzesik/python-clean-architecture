608bd7f9e93b4b84a2d922682b22d79cd30ab2ff
Piotr Grzesik <pj.grzesik@gmail.com>
2017-11-24 23:19:00 +0100

    implement test for listing memrepo withour parameters

--- tests/repository/test_memrepo.py
+++ tests/repository/test_memrepo.py
@@ -1,28 +1,34 @@
 import pytest
 
-
-@pytest.fixture
-def first_storageroom():
-    return {
-        'code': '123',
-        'size': 215,
-        'price': 39,
-        'longitude': '-0.0999999',
-        'latitude': '51.7541111'
-    }
+from rentomatic.repository.memrepo import MemRepo
 
 
-@pytest.fixture
-def second_storageroom():
-    return {
-        'code': '123456',
-        'size': 400,
-        'price': 59,
-        'longitude': '0.0999999',
-        'latitude': '51.7541111'
-    }
+class TestMemrepo:
+    @pytest.fixture
+    def first_storageroom(self):
+        return {
+            'code': '123',
+            'size': 215,
+            'price': 39,
+            'longitude': '-0.0999999',
+            'latitude': '51.7541111'
+        }
 
+    @pytest.fixture
+    def second_storageroom():
+        return {
+            'code': '123456',
+            'size': 400,
+            'price': 59,
+            'longitude': '0.0999999',
+            'latitude': '51.7541111'
+        }
 
-@pytest.fixture
-def storagerooms(first_storageroom, second_storageroom):
-    return [first_storageroom, second_storageroom]
+    @pytest.fixture
+    def storagerooms(first_storageroom, second_storageroom):
+        return [first_storageroom, second_storageroom]
+
+    def test_list_without_parameters(self, storagerooms):
+        repo = MemRepo(storagerooms)
+
+        assert repo.list() == storagerooms
